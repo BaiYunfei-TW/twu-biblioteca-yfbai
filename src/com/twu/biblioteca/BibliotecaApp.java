15ddc6e5d46a5c4a32fb5a669e0e9c34648506b6
BaiYunfei-TW <baiyunfei_tw@foxmail.com>
2018-07-13 22:56:41 +0800

    implemented BibliotecaApp

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,20 +1,16 @@
 package com.twu.biblioteca;
 
+import com.twu.biblioteca.commands.InputCommand;
+import com.twu.biblioteca.menu.MainMenu;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        new BibliotecaApp().start();
     }
 
     public void start() {
-        final String startMessage = "Hello! Welcome to Biblioteca\r\n" +
-                                    "----------------------------\r\n" +
-                                    "Menu:\r\n" +
-                                    "1. List Books\r\n" +
-                                    "2. Checkout Books\r\n" +
-                                    "3. Return Books\r\n" +
-                                    "0. Quit\r\n" +
-                                    "----------------------------\r\n";
-        System.out.print(startMessage);
+        InputCommand inputCommand = new InputCommand();
+        new MainMenu(inputCommand).enter();
     }
 }
