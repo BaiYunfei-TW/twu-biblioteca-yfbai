33903f92a006c6f0d56e18c36dd45bee94d9239b
BaiYunfei-TW <baiyunfei_tw@foxmail.com>
2018-07-15 10:56:37 +0800

    update checkout menu test

--- test/com/twu/biblioteca/menu/CheckoutRecordMenuTest.java
+++ test/com/twu/biblioteca/menu/CheckoutRecordMenuTest.java
@@ -1,25 +1,18 @@
 package com.twu.biblioteca.menu;
 
 import com.twu.biblioteca.BaseTest;
-import com.twu.biblioteca.entity.CheckoutRecord;
-import com.twu.biblioteca.repository.CheckoutRecordRepository;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class CheckoutRecordMenuTest extends BaseTest {
 
-    CheckoutRecordRepository checkoutRecordRepository = CheckoutRecordRepository.instance();
-
     @Test
     public void should_print_checked_out_records() {
         String excepted = "Type | Item details | Checkout Time | Return Time |Account | Name | Email | Tel\r\n" +
+                "----------------------------\r\n" +
+                "Book | Design Patterns.Erich Gamma.2001 | 2018-01-01 10:00:00 | - | 001-0001 | yfbai | baiyunfeiii@foxmail.com | 15626299517\r\n" +
                 "----------------------------\r\n";
-        for (CheckoutRecord record :
-                checkoutRecordRepository.checkoutButNotReturnedList()) {
-            excepted += record.toString() + "\r\n";
-        }
-        excepted += "----------------------------\r\n";
 
         CheckoutRecordMenu checkoutRecordMenu = new CheckoutRecordMenu();
         checkoutRecordMenu.enter();
